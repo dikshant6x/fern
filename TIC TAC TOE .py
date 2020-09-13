e0f867ff1f0f1682fac9d71c2007c4c29a7e5818
dikshant6x <56506621+dikshant6x@users.noreply.github.com>
2020-09-13 13:28:18 +0530

    Add files via upload

---
+++ TIC TAC TOE .py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x_o=input('what will u prefer = X or O')
+if x_o.lower()=='x':
+    p1='X'
+    p2='O'
+elif x_o.lower()=='o':
+    p1='O'
+    p2='X'
+
+    
+    
+def board(tup):
+    print(tup)
+    a=tup[0]
+    b=tup[1]
+    c=tup[2]
+    d=tup[3]
+    e=tup[4]
+    f=tup[5]
+    g=tup[6]
+    h=tup[7]
+    i=tup[8]
+    print('    |   |   ')
+    print(f'  {a} |  {b}| {c}  ')
+    print('    |   |   ')
+    print('-------------')
+    print('    |   |')
+    print(f'  {d} |  {e}| {f}')
+    print('    |   |')
+    print('-------------')
+    print('    |   |   ')
+    print(f'  {g} |  {h}| {i}   ')
+    print('    |   |   ')
+    
+    if a==b==c=='X' or a==d==g=='X' or a==e==i=='X' or b==e==h=='X' or c==e==g=='X' or c==f==i=='X' or d==e==f=='X' or g==h==i=='X':
+        result()
+        
+    elif a==b==c=='O' or a==d==g=='O'or a==e==i=='O' or b==e==h=='O' or c==e==g=='O' or c==f==i=='O' or d==e==f=='O'or g==h==i=='O':
+        result1()
+
+
+# In[2]:
+
+
+def lis1(list,position_1):
+    index1=list.index(position_1)
+    print(index1)
+    list.pop(index1)
+    print(list)
+def lis2(list,position_2):
+    index2=list.index(position_2)
+    list.pop(index2)
+    print(list)
+
+
+# In[6]:
+
+
+def p1cal(position_1,a,b,c,d,e,f,g,h,i):    
+    
+    if position_1 in [1,2,3,4,5,6,7,8,9]:
+        if position_1==1:
+            a=p1
+        elif position_1==2:
+            b=p1
+                
+        elif position_1==3:
+            c=p1
+            
+        elif position_1==4:
+             d=p1 
+               
+        elif position_1==5:
+             e=p1
+               
+        elif position_1==6:
+             f=p1
+               
+        elif position_1==7:
+             g=p1    
+              
+        elif position_1==8:
+             h=p1    
+              
+        elif position_1==9:
+             i=p1   
+    return a,b,c,d,e,f,g,h,i
+  
+               
+            
+def p2cal(position_2,tup1):
+
+    a=tup1[0]
+    b=tup1[1]
+    c=tup1[2]
+    d=tup1[3]
+    e=tup1[4]
+    f=tup1[5]
+    g=tup1[6]
+    h=tup1[7]
+    i=tup1[8]
+    if position_2 in [1,2,3,4,5,6,7,8,9]:
+        if position_2==1:
+            a=p2
+        elif position_2==2:
+             b=p2
+        elif position_2==3:
+             c=p2
+        elif position_2==4:
+             d=p2    
+        elif position_2==5:
+             e=p2
+        elif position_2==6:
+             f=p2
+        elif position_2==7:
+             g=p2    
+        elif position_2==8:
+             h=p2    
+        elif position_2==9:
+            i=p2        
+    
+
+    return a,b,c,d,e,f,g,h,i
+
+
+# In[7]:
+
+
+def main():
+    
+    position_1 = int(input('Player-1 : enter the position '))
+    print(position_1)
+    lis1(list,position_1)
+    if ctr==1:
+        
+        a=' '
+        b=' '
+        c=' '
+        d=' '
+        e=' '
+        f=' '
+        g=' '
+        h=' '
+        i=' '
+
+
+        board(p1cal(position_1,a,b,c,d,e,f,g,h,i))
+    if ctr!=1:
+        yz=xy[2]
+        a=yz[0]
+        b=yz[1]
+        c=yz[2]
+        d=yz[3]
+        e=yz[4]
+        f=yz[5]
+        g=yz[6]
+        h=yz[7]
+        i=yz[8]
+        board(p1cal(position_1,a,b,c,d,e,f,g,h,i))
+    
+    position_2 = int(input('Player-2 : enter the position '))
+    lis2(list,position_2)
+    board(p2cal(position_2,p1cal(position_1,a,b,c,d,e,f,g,h,i)))
+    xyz=p2cal(position_2,p1cal(position_1,a,b,c,d,e,f,g,h,i))
+    return position_1,position_2,xyz
+
+
+# In[ ]:
+
+
+list=[1,2,3,4,5,6,7,8,9]
+ctr=1
+while ctr<10:
+    xy=main()
+    
+    ctr=ctr+1
+    
+    def result():
+        if (x_o.lower()=='x'):
+            print('Player 1 wins')
+       
+        else:
+            print('Player 2 wins')
+        return 
+  
+        
+    def result1():
+        if (x_o.lower()=='o'):
+            print('Player 1 wins')
+        
+        else:
+            print('Player 2 wins')
+        
+    if ctr==9:
+        print('DRAW')
+        break
+
+
+# In[ ]:
+
+
+
+
---
+++ black jack new.py
@@ -0,0 +1,460 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def play_again():
+    x=input('do you want to play another hand(yes or no)')
+    if x.lower()=='yes':
+        print('NEW GAME')
+        start()
+    else :
+        print('thank you for playing ')
+        return
+    
+        
+def end():
+    while True:
+#        print('d')
+        continue
+
+
+# In[2]:
+
+
+def deck():
+    from random import randint
+    group=randint(1,4)
+    #print(group)
+    if group==1:
+        e=hearts()
+    if group==2:
+        e=clubs()
+    if group==3:
+        e=spades()
+    if group==4:
+        e=diamonds()
+    return e
+
+
+# In[3]:
+
+
+list1=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+list2=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+list3=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+list4=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+def lis1(rand):
+    ind=list1.index(rand)
+    list1.pop(ind)
+def lis2(rand):
+    ind=list2.index(rand)
+    list2.pop(ind)
+def lis3(rand):
+    ind=list3.index(rand)
+    list3.pop(ind)
+def lis4(rand):
+    ind=list4.index(rand)
+    list4.pop(ind)
+    
+from random import randint    
+    
+def hearts():
+    h=randint(1,13)
+    #print(h)
+    gr='hearts'
+    if h in list1:
+        lis1(h)
+        #print(list1)
+        if h==1:
+            print(gr+' ace')    
+        elif h==2:
+            print(f'{gr} {h}')
+        
+        elif h==3:
+            print(f'{gr} {h}') 
+            
+        elif h==4:
+            print(f'{gr} {h}') 
+           
+        elif h==5:
+            print(f'{gr} {h}')
+         
+        elif h==6:
+            print(f'{gr} {h}')
+        
+        elif h==7:
+            print(f'{gr} {h}')
+    
+        elif h==8:
+            print(f'{gr} {h}')
+               
+        elif h==9:
+            print(f'{gr} {h}') 
+                
+        elif h==10:
+            print(f'{gr} {h}') 
+                 
+        elif h==11:
+            print(gr+' jack')  
+            h=10
+        elif h==12:
+            print(gr+' queen') 
+            h=10
+        elif h==13:
+            print(gr+' king')
+            h=10
+    else:
+        deck()    
+    return h    
+def clubs():
+    h=randint(1,13)
+    #print(h)
+    gr='clubs'
+    if h in list2:
+        lis2(h)
+        #print(list2)
+        if h==1:
+            print(gr+' ace')
+            
+        elif h==2:
+            print(f'{gr} {h}')
+            
+        elif h==3:
+            print(f'{gr} {h}')
+                
+        elif h==4:
+            print(f'{gr} {h}')
+               
+        elif h==5:
+            print(f'{gr} {h}')
+            
+        elif h==6:
+            print(f'{gr} {h}')
+            
+        elif h==7:
+            print(f'{gr} {h}')
+            
+        elif h==8:
+            print(f'{gr} {h}')
+            
+        elif h==9:
+            print(f'{gr} {h}')
+                
+        elif h==10:
+            print(f'{gr} {h}')
+            
+        elif h==11:
+            print(gr+' jack') 
+            h=10
+        elif h==12:
+            print(gr+' queen')
+            h=10  
+        elif h==13:
+            print(gr+' king')  
+            h=10
+    else:
+        deck() 
+    return h    
+def spades():
+    h=randint(1,13)
+    #print(h)
+    gr='spades'
+    if h in list3:
+        lis3(h)
+        #print(list3)
+        if h==1:
+            print(gr+' ace')
+        elif h==2:
+            print(f'{gr} {h}')
+        elif h==3:
+            print(f'{gr} {h}')  
+        elif h==4:
+            print(f'{gr} {h}') 
+        elif h==5:
+            print(f'{gr} {h}')
+        elif h==6:
+            print(f'{gr} {h}')
+        elif h==7:
+            print(f'{gr} {h}')
+        elif h==8:
+            print(f'{gr} {h}')
+        elif h==9:
+            print(f'{gr} {h}') 
+        elif h==10:
+            print(f'{gr} {h}')
+        elif h==11:
+            print(gr+' jack')
+            h=h-1
+        elif h==12:
+            print(gr+' queen')
+            h= h-2
+        elif h==13:
+            print(gr+' king')
+            h= h-3
+    else:
+        deck()
+    return h    
+def diamonds():
+    h=randint(1,13)
+    #print(h)
+    gr='diamonds'
+    
+    if h in list4:
+        lis4(h)
+        #print(list4)
+        if h==1:
+            print(gr+' ace')
+            return h
+        elif h==2:
+            print(f'{gr} {h}')
+            return h
+        elif h==3:
+            print(f'{gr} {h}')  
+            return h
+        elif h==4:
+            print(f'{gr} {h}') 
+            return h
+        elif h==5:
+            print(f'{gr} {h}')
+            return h
+        elif h==6:
+            print(f'{gr} {h}')
+            return h
+        elif h==7:
+            print(f'{gr} {h}')
+            return h
+        elif h==8:
+            print(f'{gr} {h}')
+        elif h==9:
+            print(f'{gr} {h}') 
+            return h
+        elif h==10:
+            print(f'{gr} {h}')   
+            return h
+        elif h==11:
+            print(gr+' jack')    
+            return h-1
+        elif h==12:
+            print(gr+' queen')  
+            h= h-2
+        elif h==13:
+            print(gr+' king') 
+            h= h-3
+    else:
+        deck()       
+    return h    
+
+
+# In[4]:
+
+
+class dealer():
+    def __init__(self):
+        pass
+    def hit(self):
+        ds=deck()
+        return ds
+    
+class player():
+    def __init__(self,chipbalance):
+        self.chipb=chipbalance
+        pass
+    def chiploss(self,chips=2):
+        ub=self.chipb-chips
+        self.chipb=ub
+        return ub
+            
+            
+    def hit(self):
+        ps=deck()
+        return ps
+    def chipwin(self,chips=2):
+        ub=self.chipb+chips
+        self.chipb=ub
+        return ub
+    
+
+    
+def ace():
+    print('ace is here')
+    f=int(input('what do you want to choose(1 or 11)'))
+    if f==1 or f==11:
+        pass
+    else:
+        ace()
+    return f   
+        
+def check(number):
+    if number==1:
+        ace()
+    else:
+        pass
+
+
+# In[5]:
+
+
+de=dealer()
+pl=player(10)
+
+
+# In[6]:
+
+
+def start():
+    print('Welcome to BlackJack! Get as close to 21 as you can without going over!\n    Dealer hits until she reaches 17. Aces count as 1 or 11.')
+    print('PLAYER CARDS')
+    p1=pl.hit()
+    if p1==1:
+        p1=ace()
+#    print(p1)
+    p2=pl.hit()
+    if p2==1:
+        p2=ace()
+#    print(p2)
+    print('--'*20)
+    print('DEALER CARDS')
+    d1=de.hit()
+    if d1==1:
+        d1=ace()
+#    print(d1)
+    print('face down')
+    print('--'*20)
+    #print(type(p1))
+    #print(type(p2))
+    total=p1+p2
+    print(f'player card_total_is {total}')
+    while True:    
+        if total<21:
+            a=input('Player:want to hit(yes or no) ')
+            if a.lower()=='yes':
+                print('PLAYER')
+                p3=pl.hit()
+                if p3==1:
+                    p3=ace()
+                total=total+p3
+                print(total)
+            elif a.lower()=='no':
+                break
+            else:
+                print('try again')
+        elif total==21:
+            break
+        else:
+            print('PLAYER BUSTED')
+            print('-'*20)
+            pl.chiploss()
+            if pl.chipb>1:
+                play_again()
+            else:
+                end()
+                
+            
+            #break
+    print('-'*20)        
+    print('DEALER')
+    d2=de.hit()
+    if d2==1:
+        d2=ace()
+#    print(d2)
+    tot=d1+d2
+    print(tot)
+    while True:
+        if tot<17:  #dealer total less than 17 case
+            d3=de.hit()
+            if d3==1:
+                d3=ace()
+#            print(d3)
+            tot=tot+d3
+            print(tot)
+        elif tot>=17 and tot<=total:
+            d4=de.hit()
+            if d4==1:
+                d4=ace()
+#            print(d4)
+            tot=tot+d4
+            print(tot)
+#            b=input('dealer:want to hit')
+#            if b.lower()=='yes':
+#                if tot>total and tot<=21:
+#                    print('Dealer wins')
+#                    break
+#                elif tot>21:
+#                    print('Dealer busted')
+#                    break
+        elif tot>total and tot<=21:
+            print('DEALER WINS')
+            print(' '*20)
+            pl.chiploss()
+            if pl.chipb>1:
+                play_again()
+            #break
+        elif tot>21:
+            print('DEALER BUSTED')
+            print('-'*20)
+            pl.chipwin()
+            if pl.chipb>1:
+                play_again()
+            
+            break
+
+            
+            
+            
+
+
+# In[ ]:
+
+
+start()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
